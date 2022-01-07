a2810f3e1d52b1af296f938998b37420ca80d304
seungbeom1010 <seungbeom.dev@gmail.com>
2022-01-08 00:16:29 +0900

    RN연습(간단 기능 구현 및 스타일링)

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { disableExpoCliLogging } from 'expo/build/logs/Logs';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
 
 export default function App() {
+
+  const [cnt, setCnt] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Number Counter</Text>
+
+      <Text style={styles.textTitle}>Current Number</Text>
+
+      <Text style={styles.cnt}>{cnt}</Text>
+
+      <View style={styles.buttonBox}>
+        <View style={styles.button1}>
+          <Button color='white' stlye={styles.button1} title='더하기' onPress={ () => setCnt(cnt + 1) }/>
+        </View>
+        <View style={styles.button2}>
+          <Button color='white' stlye={styles.button1} title='빼 기' onPress={ () => setCnt(cnt - 1) }/>
+        </View>
+      </View>
+      
+      <View style={styles.check}>
+        <Button color='white' title='Check' onPress={ () => Alert.alert('현재 숫자는 ' + cnt + ' 입니다.') }/>
+      </View>
+      
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -13,8 +38,59 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
+    flexDirection:'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    textAlign: 'center',
+    paddingVertical: 20,
+    color: 'white',
+    width: '90%',
+    backgroundColor: 'pink',
+    fontSize: 35,
+    marginBottom: 30
+  },
+  textTitle:{
+    textAlign: 'center',
+    backgroundColor: 'white',
+    marginBottom: 15,
+    fontSize: 25
+  },
+  cnt:{
+    fontSize: 50,
+    fontWeight: '300',
+    borderColor: 'black',
+    borderWidth: 2,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderRadius: 20
+
+  },
+  buttonBox:{
+    marginTop: 30,
+    flexDirection: 'row',
+    width: '80%',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  button1:{
+    backgroundColor: 'red',
+    width: '30%',
+    height: 40,
+    borderWidth: 2,
+    borderColor: 'black'
+  },
+  button2:{
+    backgroundColor: 'blue',
+    width: '30%',
+    borderWidth: 2,
+    borderColor: 'black'
+  },
+  check:{
+    marginVertical: 20,
+    backgroundColor: 'skyblue'
+  }
 });
